d3d0e7309e067aa880ec93198bba9bdf212e236a
mike-shtil <mike002@gmail.com>
2016-04-16 05:44:59 +0300

    basic HTML+CSS done

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+//default task
+gulp.task('default', ['scss', 'watch']);
+
+//watch Files For Changes
+gulp.task('watch', function() {
+  $.livereload.listen();
+  gulp.watch('features/**/*.scss', ['scss']);
+});
+
+//SASS build process
+gulp.task('scss', function() {
+   gulp.src('features/**/*.scss')
+      .pipe($.plumber())
+      .pipe($.sourcemaps.init())
+      .pipe($.sass({errLogToConsole: true, sourcemap: true, style: 'compact'}))
+      .pipe($.autoprefixer({browsers: ['last 1 version', 'iOS 6'], cascade: false}))
+      .pipe($.sourcemaps.write({includeContent: true, sourceRoot: '.'}))
+      //.pipe(cssnano({discardDuplicates: false})) //optional minification
+      .pipe(gulp.dest('features'))
+      .pipe($.livereload())
+});
